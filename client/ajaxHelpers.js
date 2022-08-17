31a1a14e2a7c8387025505d32d78ebfcae300754
Corum Gold <goldcorum@gmail.com>
2022-08-17 17:42:58 -0400

    Make main page show all puppies

--- client/ajaxHelpers.js
+++ client/ajaxHelpers.js
@@ -1,17 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
+//This runs the get request from players.js
 export const fetchAllPlayers = async () => {
+  try {
+    const players = await axios.get("/api/players");
+    return players.data;
+  } catch (err) {
+    console.error("Couldn't fetch the players!");
+  }
+};
 
-}
+export const fetchSinglePlayer = async (playerId) => {};
 
-export const fetchSinglePlayer = async (playerId) => {
+export const addNewPlayer = async (playerObj) => {};
 
-}
-
-export const addNewPlayer = async (playerObj) => {
-
-}
-
-export const removePlayer = async (playerId) => {
-
-}
+export const removePlayer = async (playerId) => {};
